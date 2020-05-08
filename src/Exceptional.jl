b626ba91f88cf7287bce16bfd3955f1dbf97e2e1
Manuel Sousa <manuelsousamvs@gmail.com>
2020-05-08 17:29:56 +0100

    Add handler_bind and redefinition of error()

--- src/Exceptional.jl
+++ src/Exceptional.jl
@@ -36,23 +36,66 @@ function return_form(func, value = nothing)
 end
 
 
+# Tunned example from project spec
 
-mystery(n) =
-    1 +
-    block() do outer
-        1 +
-        block() do inner
-            1 +
-            block() do innerx2
-                1 +
-                if n == 0
-                    return_form(inner,11)
-                elseif n == 1
-                    return_form(outer,1)
-                else
-                    return_form(innerx2,2)
-                end
+# mystery(n) =
+#     1 +
+#     block() do outer
+#         1 +
+#         block() do inner
+#             1 +
+#             block() do innerx2
+#                 1 +
+#                 if n == 0
+#                     return_form(inner,11)
+#                 elseif n == 1
+#                     return_form(outer,1)
+#                 else
+#                     return_form(innerx2,2)
+#                 end
+#             end
+#         end
+#     end
+# end
+
+import Base.error
+
+function error(ex)
+    throw(ex)
+end
+
+reciprocal(x) =
+  x == 0 ?
+    error(DivisionByZero()) :
+    1/x
+
+struct DivisionByZero <: Exception end
+
+# Base.showerror(io::IO, e::DivisionByZero) = print(io, e.msg)
+
+dict = Dict()
+
+function handler_bind(func,args...)
+    try
+        func()
+    catch e
+        for i in args
+            if  isa(e,i.first)
+                i.second(i)
             end
         end
+        rethrow()
     end
 end
+
+
+handler_bind(()->reciprocal(0), DivisionByZero =>(c)->println("I saw a division by zero"))
+
+
+handler_bind(DivisionByZero =>
+            (c)->println("I saw it too")) do
+                handler_bind(DivisionByZero =>
+                    (c)->println("I saw a division by zero")) do
+                        reciprocal(0)
+                    end
+       end

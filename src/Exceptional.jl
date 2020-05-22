be963c2fb5fb5195043fc124780fae3719e534bd
Manuel Sousa <manuelsousamvs@gmail.com>
2020-05-22 06:02:03 +0100

    Add comments 2

--- src/Exceptional.jl
+++ src/Exceptional.jl
@@ -23,7 +23,6 @@ function block(f)
     try
         f(f)
     catch e
-        println(typeof(f))
         typeof(e) == ReturnFromException && f === e.func ? e.value : throw(e)
     end
 end
@@ -31,8 +30,8 @@ end
 # Function    : return_from
 # Description : throws ReturnFromException, created to go up the call chain
 #               and alert a block() when a return_from is called
-# Arguments   : func-> function of the exit point which identifies a block().
-#               value-> value to be returned by the corresponding block
+# Arguments   : func -> function of the exit point which identifies a block().
+#               value -> value to be returned by the corresponding block
 function return_from(func, value = nothing)
     throw(ReturnFromException(func,value))
 end
@@ -58,8 +57,8 @@ end
 
 
 # Function    : handler_bind
-# Arguments   : func-> a function (or code in this context)
-#               value-> list of handlers to associated with arg func
+# Arguments   : func -> a function (or code in this context)
+#               value -> list of handlers to associated with arg func
 # Description : This function executes the func arg code and waits for an exception
 #               to be thrown by signal() (eg. ReturnFromException)
 #               This function also makes proper handling of handlers_stack operations (handlers context)
@@ -81,7 +80,7 @@ end
 
 
 # Function    : find_handler
-# Arguments   : e-> exception to search in handlers_stack
+# Arguments   : e -> exception to search in handlers_stack
 # Description : This function grabs the current handlers, and returns the proper one for the "e" exception.
 #               It assumes the handlers_stack is providing the correct handlers context.
 function find_handler(e)
@@ -97,15 +96,15 @@ end
 
 
 # Function    : signal
-# Arguments   : e-> exception to handle
+# Arguments   : e -> exception to handle
 # Description : Recursive function to propagate a signal through all the interested handlers
-#               It is responsible to update handlers_stack, keeping the correct handlers context
-#               between recursive calls, restoring the context stack before throwing back to [handler,restart]_bind
+#               It is responsible to update handlers_stack, keeping the correct context stack
+#               between recursive calls, restoring it before throwing back to [handler,restart]_bind
 function signal(e)
     callback = find_handler(e)
     if callback != nothing
         # This callback can call a return_from or invoke_restart, which will throw the corresponding Exception
-        # We do not try-catch this callback here as a design choise, thus, all exceptions will be sent to
+        # We do not try-catch this callback here as a design choise, thus, all exceptions will be thrown to
         # the corresponding [handler,restart]_bind which might want to trigger very specifc actions uppon receiving them
         callback(e)
         if length(handlers_stack) > 1
@@ -117,7 +116,6 @@ function signal(e)
                 throw(ee)
             end
         else
-            println("I would like to knwo when you are called")
             throw(e)
         end
     else
@@ -125,9 +123,19 @@ function signal(e)
     end
 end
 
-function restart_bind(func,args...)
+
+# Function    : restart_bind
+# Arguments   : func ->  function (or code in this context)
+#             : restart_functions ->  list of restart functions to associated with arg func
+# Description : This function has a similiar behaviour as handler_bind, first executing the func code
+#               and waiting for exceptions to be thrown. If something is catched, it will signal the Exception to
+#               call the atention of interested handlers. After signaling this function waits for possible
+#               invoke restarts to be called, returning their execution to the regular program flow
+#               It is also reaponsible to handle the context stack of restart functions from handler to handler
+#               keeping the right stack to the right binding.
+function restart_bind(func,restart_functions...)
     tmp = Dict()
-    for i in args
+    for i in restart_functions
         tmp[i.first] = i.second
     end
     append!(restarts_stack,[tmp])
@@ -142,8 +150,12 @@ function restart_bind(func,args...)
                 if e.name ∉ keys(restarts_stack[end])
                     pop!(restarts_stack)
                     if isempty(restarts_stack)
+                        # it reached the top of the call chain
+                        # there are no more restart_bind calls, meaning the restart function does not exist
                         throw(NoRestartExistException())
                     else
+                        # lets throw it again, to move up in the call chain
+                        # another restart_bind might have the restart function defined
                         throw(e)
                     end
                 else
@@ -159,12 +171,23 @@ function restart_bind(func,args...)
     return return_value
 end
 
+
+# Function    : invoke_restart
+# Arguments   : symbol ->  a symbol to identify a restart function
+#             : args ->  list of args of the "symbol" restart function
+# Description : Throws an Invoke Restart Exception so it can be distinguished
+#               along the call chain. Contains the necessary information to identify
+#               and get the restart function from the restarts stack
 function invoke_restart(symbol, args...)
     throw(InvokeRestartException(symbol,args))
 end
 
 
-
+# Function    : available_restart
+# Arguments   : name -> name of the restart function to search
+# Description : This function go through all available restarts for a certain context
+#               and returns a boolean it (arg name) is available
+#               It assumes the restarts_stack is providing the correct restarts context.
 function available_restart(name)
     for i in restarts_stack
         if name in keys(i)
